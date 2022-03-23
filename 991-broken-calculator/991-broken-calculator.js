a102ee43b0e4dbc5e2c10d26d2c6ae23ffe677ef
Bruno Leonardo <brunoleo223@gmail.com>
2022-03-23 14:54:27 -0300

    Time: 95 ms (63.64%), Space: 42 MB (57.58%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.js
+++ 991-broken-calculator/991-broken-calculator.js
@@ -7,7 +7,7 @@ var brokenCalc = function(startValue, target) {
     let count = 0;
     
     while(target > startValue){
-        if(target % 2 == 0){
+        if(target % 2 === 0){
             target /= 2;
         } else {
             target += 1;
