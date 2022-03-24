4d4635d3e5e9827030207c3134245c374fdd3f4f
Bruno Leonardo <brunoleo223@gmail.com>
2022-03-24 20:07:53 -0300

    Time: 274 ms (19.43%), Space: 49.9 MB (71.22%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people.sort((a, b) => a - b);
+    var j = people.length - 1, i = 0;
+    var boats = 0;
+    while(i <= j){
+        
+        if(people[i] + people[j] <= limit){
+            ++i;
+        }
+        
+        --j;
+        boats++;
+    }
+    return boats;
+};
