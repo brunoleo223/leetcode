9a52d825d30376f931bcc6858e60b65adad8a2f7
Bruno Leonardo <brunoleo223@gmail.com>
2022-03-25 20:48:45 -0300

    Time: 99 ms (46.38%), Space: 42.3 MB (84.06%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.js
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.js
@@ -4,45 +4,44 @@
  * @return {number[]}
  */
 var decrypt = function(code, k) {
-    const n = code.length;
+    let result = 0, sum = 0;
+    if( k === 0){
+        return new Array(code.length).fill(0);
+    }
     
-    if (k === 0) return new Array(n).fill(0);
     
-    const res = new Array(n).fill(0);
-    
-    if (k > 0) {
-        let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += code[i];
+    }
+    result = []
+    if( k > 0 ){
         
-        for (let i = 0; i < k; i++) {
-            sum += code[i];
-        }
         
-        for (let i = 0; i < n; i++) {
-            sum += code[(i + k) % n];
-            sum -= code[i];
+        for(let i = 0; i < code.length; i++){
+            sum += code[(i + k) % code.length]
+            sum -= code[i]
             
-            res[i] = sum;
+            result[i] = sum;
         }
-        
-        return res;
     }
     
     if (k < 0) {
         k = -k;
         
-        let sum = 0;
+        sum = 0;
         
         for (let i = 0; i < k; i++) {
             sum += code[i];
         }
         
-        for (let i = 0; i < n; i++) {
-            res[(i + k) % n] = sum;
+        for (let i = 0; i < code.length; i++) {
+            result[(i + k) % code.length] = sum;
             
             sum -= code[i];
-            sum += code[(i + k) % n];
+            sum += code[(i + k) % code.length];
         }
         
-        return res;
     }
+        
+    return result;
 };
