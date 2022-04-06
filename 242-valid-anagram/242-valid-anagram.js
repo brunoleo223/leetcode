fd11e3637678c47e488008ecaff24c322b113f42
Bruno Leonardo <brunoleo223@gmail.com>
2022-04-06 18:26:51 -0300

    Time: 130 ms (32.86%), Space: 48.1 MB (21.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,22 +4,8 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    
+    return s.split('').sort().join('') === t.split('').sort().join('')
 
-  let countS = 0;
-  let countT = 0;
-  
-  const arrayS = Array.from(s);
-  const arrayT = Array.from(t);
-  
-  arrayS.sort()
-  arrayT.sort()
-  
-  if(arrayS.length != arrayT.length) return false
-  
-  for(let i = 0; i < s.length; i++){
-  	if(arrayS[i] !== arrayT[i]) return false
-  }
-  
-  return true
 
 };
