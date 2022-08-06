ace5b579a4547581af0045a3adbd21bae0361040
Bruno Leonardo <brunoleo223@gmail.com>
2022-08-05 23:18:20 -0300

    Time: 77 ms (90.38%), Space: 42.9 MB (38.70%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let hash = {};
+      for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (hash[diff] !== undefined) {
+          return [hash[diff], i];
+        }
+        hash[nums[i]] = i;
+      }
+};
