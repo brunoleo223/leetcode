ac9b643bbe6a94b8a84abe670c9bb68e8b6848fa
Bruno Leonardo <brunoleo223@gmail.com>
2022-09-26 23:03:14 -0300

    Time: 92 ms (47.87%), Space: 41.9 MB (78.72%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.js
+++ 1507-reformat-date/1507-reformat-date.js
@@ -3,13 +3,9 @@
  * @return {string}
  */
 var reformatDate = function(date) {
-    let temp = date.split(' ');
-    
-    const month = new Map([["Jan", '01'], ["Feb", '02'], ["Mar", '03'], ["Apr" , '04'], 
-                      ["May" , '05'], ["Jun" , '06'], ["Jul" , '07'], ["Aug", '08'],
-                      ["Sep", '09'], ["Oct" , '10'], [ "Nov", '11'], ["Dec", '12']]);
-    
-    const day = temp[0].replace(/[^0-9]/g, '');
-    
-    return temp[2] + '-' + month.get(temp[1]) + '-' + (day.length > 1 ? day : '0' + day);
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    let year = date.slice(date.length - 4), month = date.slice(date.length - 8, date.length - 5), i = 0
+    while(/\d/.test(date[i])) i++
+    let day = date.slice(0,i)
+    return year + '-' + String((months.indexOf(month) + 1)).padStart(2,'0') + '-' + day.padStart(2,'0')
 };
